df13d16cf6cf01bc31a9be776092a6f8cfe1c78c
Vladimir Cezar <vladimircezar@icloud.com>
2023-12-05 17:35:19 -0500

    Add feature to delete mood entry

--- MoodTracker Watch App/Screens/MoodHistoryScreen.swift
+++ MoodTracker Watch App/Screens/MoodHistoryScreen.swift
@@ -3,10 +3,26 @@ import SwiftData
 
 struct MoodHistoryScreen: View {
   @Query var list: [MoodEntry]
+  @Environment(\.modelContext) private var context
+  
   var body: some View {
-    List {
-      ForEach(list, id: \.id) { entry in
-        MoodRowView(mood: entry.mood)
+    if list.isEmpty {
+      ContentUnavailableView(
+        "Nothing, Zit, Nada",
+        image: "doc.richtext.fill",
+        description: Text("This place is too clean...")
+      )
+    } else {
+      List {
+        ForEach(list, id: \.id) { entry in
+          MoodRowView(mood: entry.mood)
+        }
+        .onDelete(perform: { indexSet in
+          for i in indexSet {
+            let entry = list[i]
+            context.delete(entry)
+          }
+        })
       }
     }
   }
